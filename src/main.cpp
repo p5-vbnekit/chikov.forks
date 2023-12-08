d2f888fe714b63f3ba585611be34c8e317ae3328
Nikita Pushchin <vbnekit@p5y.su>
2023-12-08 22:45:17 +0300

    src/main.cpp - ugly windows-way console manipulations
    It's unnecessary and ugly, but some Windows console applications wait for any key when exiting...
    This breaks the entire paradigm of `CLI` (command-line interface) at the root.
    And for no small number of reasons, `system("pause")` is one of the worst evils we can do in this case.
    In addition, working with the (user|virtual|etc) terminal is OS-specific.
    The task is currently poorly supported by the standard c++ library.
    However, to support "backwards compatibility" we can add an interactive mode with `wait_for_any_key_` function (under `ifdef _WIN32` guard) via `::GetStdHandle` & `::ReadConsoleInput` from `WINAPI`.
    Note, the quest is not as simple as it might seem at first glance, `wait_for_any_key_` is still not fully integrated with the O.S. specific:
    - In essence, this will be reinventing the wheel - not the best task for a pure c++. Better to use `Qt`, `boost` or something for this.
    - It is better to determine the owner of the terminal (is our program launched “through a click” or normally).
    - Not all terminations will be handled now, it is better to handle real “at exit” case.
    - Termination of the process can be performed from another thread with hardcoded 5 sec. timeout!!!
    - And there will still be many pitfalls...
    
    P.S: Poor windows application developers... =(

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <stdexcept>
+
+#ifdef _WIN32
+#include <windows.h>
+
+inline static auto wait_for_any_key_() noexcept(false) {
+    auto const source_ = ::GetStdHandle(STD_INPUT_HANDLE);
+    if (NULL == source_) throw ::std::runtime_error{"NULL == ::GetStdHandle(STD_INPUT_HANDLE)"};
+
+    ::std::clog << "Press any key to exit" << ::std::endl << ::std::flush;
+
+    while (true) {
+        ::INPUT_RECORD buffer_;
+        ::DWORD received_count_ = 0;
+        constexpr static ::DWORD const expected_count_ = 1;
+        if (0 == ::ReadConsoleInput(
+            source_, &buffer_, expected_count_, &received_count_
+        )) throw ::std::runtime_error{"0 == ::ReadConsoleInput(...)"};
+        if (expected_count_ != received_count_) throw ::std::runtime_error{"expected_count_ != received_count_"};
+        if (KEY_EVENT == buffer_.EventType) break;
+    }
+}
+#endif // _WIN32
 
 int main(int, char **) {
     ::std::cout << "Ok" << ::std::endl << ::std::flush;
-    ::system("pause");
+
+#ifdef _WIN32
+    ::wait_for_any_key_();
+#endif // _WIN32
+
     return 0;
 }
